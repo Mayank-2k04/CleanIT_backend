1c0d4233cd8e3d2ad16a7550b4148eb583d9103f
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-03-28 00:01:10 +0530

    modify token routes, add role extraction from payload, create role based authentication

--- myapi/auth/authorization.py
+++ myapi/auth/authorization.py
@@ -1,5 +1,3 @@
-
-
 from myapi.config import SECRET_KEY, ALGORITHM
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -7,11 +5,10 @@ from jose import jwt, JWTError
 
 # OAuth2 for different user roles
 student_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/student/login/verify-otp")
-worker_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/worker/login/verify-otp")
-admin_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/admin/login/verify-otp")
+staff_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/staff/login/verify-otp")
 
 # Common function to decode JWT for any role
-def get_current_user(token: str, oauth2_scheme: OAuth2PasswordBearer):
+def get_current_user(token: str):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid or expired token",
@@ -19,21 +16,19 @@ def get_current_user(token: str, oauth2_scheme: OAuth2PasswordBearer):
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_identity = payload.get("sub")  # `sub` contains phone number (or email for students)
+        user_identity = payload.get("sub")
+        user_role = payload.get("role")# `sub` contains phone number (or email for students)
 
         if user_identity is None:
             raise credentials_exception
-        return user_identity
+        return {"id":user_identity, "role":user_role}
 
     except JWTError:
         raise credentials_exception
 
 # Role-based authentication
 def get_current_student(token: str = Depends(student_oauth2_scheme)):
-    return get_current_user(token, student_oauth2_scheme)
+    return get_current_user(token)
 
-def get_current_worker(token: str = Depends(worker_oauth2_scheme)):
-    return get_current_user(token, worker_oauth2_scheme)
-
-def get_current_admin(token: str = Depends(admin_oauth2_scheme)):
-    return get_current_user(token, admin_oauth2_scheme)
+def get_current_worker(token: str = Depends(staff_oauth2_scheme)):
+    return get_current_user(token)
