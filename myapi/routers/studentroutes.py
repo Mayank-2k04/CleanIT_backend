06718a13ed58e6725ac06065fd8ce1e81c7be55c
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-03-28 03:00:41 +0530

    Add create request route, create request logic

--- myapi/routers/studentroutes.py
+++ myapi/routers/studentroutes.py
@@ -1,4 +1,22 @@
 #TODO add route to create a request
+from .. import schemas,databaseconnect
+from sqlalchemy.orm import Session
+from fastapi import APIRouter,Depends
+from myapi.auth import authorization
+from myapi.student import querylogics
+
+router = APIRouter(
+    prefix="/student"
+)
+
+@router.post("/request")
+def create_request(
+        request: schemas.Request,
+        user_detail: dict=Depends(authorization.get_current_student),
+        db: Session=Depends(databaseconnect.get_db)
+):
+    return querylogics.create_request(request, db, user_detail)
+
 
 #TODO add route to view current request
 
---
+++ myapi/student/querylogics.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from .. import schemas, models, databaseconnect
+from fastapi import HTTPException,status
+from sqlalchemy.orm import Session
+
+
+
+
+def create_request(
+        request: schemas.Request,
+        db: Session,
+        user_detail: dict
+                   ):
+    student_email = user_detail["id"]
+    student = db.query(models.Student).filter(models.Student.email == student_email).first()
+    s_r_id = student.r_id
+
+    old_req = db.query(models.Request).filter(
+        models.Request.r_id == s_r_id,
+        models.Request.progress == "pending"
+    ).first()
+
+    if old_req:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Pending request found!")
+
+        # Convert deadline from string to datetime
+    try:
+        deadline_datetime = datetime.fromisoformat(request.deadline)  # Expecting an ISO 8601 formatted string
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid deadline format. Use ISO 8601 format (YYYY-MM-DDTHH:MM:SS)"
+        )
+
+    new_request = models.Request(
+        r_id=s_r_id,
+        assigned_time=datetime.now(),
+        deadline=deadline_datetime,
+        progress="pending"
+    )
+    db.add(new_request)
+    db.commit()
+    db.refresh(new_request)
+    return {
+        "message": "Request created successfully",
+        "request_id": new_request.request_id,
+        "room_id": s_r_id
+    }
