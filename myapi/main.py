148d4f829d251e283f8e82c7cdb1e520cc05906e
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-03-28 03:01:26 +0530

    Add student routes

--- myapi/main.py
+++ myapi/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .databaseconnect import engine
-from .routers import studentlogin, userpoplulation, staff_login, adminroutes
+from .routers import studentlogin, userpoplulation, staff_login, adminroutes, studentroutes
 
 app = FastAPI()
 models.base.metadata.create_all(engine)
@@ -10,6 +10,7 @@ app.include_router(studentlogin.router)
 app.include_router(userpoplulation.router)
 app.include_router(staff_login.router)
 app.include_router(adminroutes.router)
+app.include_router(studentroutes.router)
 
 #make schemas for requests
 
