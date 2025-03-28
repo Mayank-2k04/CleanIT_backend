e3d5dbb0a8ce70925ffd4321dac23ef0e301f6e2
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-03-28 20:32:34 +0530

    add routes for student details and history

--- myapi/routers/studentroutes.py
+++ myapi/routers/studentroutes.py
@@ -1,9 +1,9 @@
-#TODO add route to create a request
 from .. import schemas,databaseconnect
 from sqlalchemy.orm import Session
 from fastapi import APIRouter,Depends
 from myapi.auth import authorization
 from myapi.student import querylogics
+from typing import List
 
 router = APIRouter(
     prefix="/student",
@@ -20,12 +20,23 @@ def create_request(
     return querylogics.create_request(request, db, user_detail)
 
 
-@router.get("/request",response_model=schemas.DisplayRequest)
+@router.get("/request",response_model=schemas.DisplayCurrentRequest)
 def get_request(
         user_detail: dict=Depends(authorization.get_current_student),
         db: Session=Depends(databaseconnect.get_db)
 ):
     return querylogics.get_request(user_detail, db)
 
+@router.get("/details", response_model=schemas.DisplayStudent)
+def get_details(
+        user_detail: dict=Depends(authorization.get_current_student),
+        db: Session=Depends(databaseconnect.get_db)
+):
+    return querylogics.get_details(user_detail, db)
 
-#TODO add route to view previous requests history
+@router.get("/history", response_model=List[schemas.History])
+def history(
+        user_detail: dict=Depends(authorization.get_current_student),
+        db: Session=Depends(databaseconnect.get_db)
+):
+    return querylogics.history(user_detail, db)
